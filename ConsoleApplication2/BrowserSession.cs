7ec547eed74010b6975c4bdd095dfd7e5258a529
DESKTOP-3A4BHLQ\junic <junicolas@gmail.com>
2019-07-21 01:05:16 +0200

    initial commit

---
+++ ConsoleApplication2/BrowserSession.cs
@@ -0,0 +1,209 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Web;
+using HtmlAgilityPack;
+
+public class BrowserSession
+{
+    private bool _isPost;
+    private HtmlDocument _htmlDoc;
+    private bool _isPostXhr;
+   
+    private bool _capture;
+
+    /// <summary>
+    /// System.Net.CookieCollection. Provides a collection container for instances of Cookie class 
+    /// </summary>
+    public CookieCollection Cookies { get; set; }
+
+    /// <summary>
+    /// Provide a key-value-pair collection of form elements 
+    /// </summary>
+    public FormElementCollection FormElements { get; set; }
+
+    /// <summary>
+    /// Makes a HTTP GET request to the given URL
+    /// </summary>
+    public string Get(string url)
+    {
+        _isPost = false;
+        _capture = true;
+        _isPostXhr = false;
+
+        CreateWebRequestObject().Load(url);
+        return _htmlDoc.DocumentNode.InnerHtml;
+    }
+
+    /// <summary>
+    /// Makes a HTTP POST request to the given URL
+    /// </summary>
+    public string Post(string url)
+    {
+        _capture = false;
+        _isPost = true;
+        _isPostXhr = false;
+        
+        CreateWebRequestObject().Load(url, "POST");
+        return _htmlDoc.DocumentNode.InnerHtml;
+    }
+
+    public string PostXhr(string url)
+    {
+        _isPost = true;
+        _isPostXhr = true;
+        _capture = true;
+
+        CreateWebRequestObject().Load(url, "POST");
+        return _htmlDoc.DocumentNode.InnerHtml;
+    }
+
+    /// <summary>
+    /// Creates the HtmlWeb object and initializes all event handlers. 
+    /// </summary>
+    private HtmlWeb CreateWebRequestObject()
+    {
+        HtmlWeb web = new HtmlWeb();
+        web.UseCookies = true;
+        web.CaptureRedirect = _capture;
+        web.PreRequest = new HtmlWeb.PreRequestHandler(OnPreRequest);
+        web.PostResponse = new HtmlWeb.PostResponseHandler(OnAfterResponse);
+        web.PreHandleDocument = new HtmlWeb.PreHandleDocumentHandler(OnPreHandleDocument);
+        return web;
+    }
+
+    /// <summary>
+    /// Event handler for HtmlWeb.PreRequestHandler. Occurs before an HTTP request is executed.
+    /// </summary>
+    protected bool OnPreRequest(HttpWebRequest request)
+    {
+       // request.Headers["Accept-Encoding"] = "gzip, deflate, br";
+        request.Accept =
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3";
+
+        request.UserAgent =
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36";
+        request.Headers["Accept-Language"] =
+            "Accept-Language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6";
+        AddCookiesTo(request); // Add cookies that were saved from previous requests
+        if (_isPostXhr)
+        {
+            request.Headers["X-Requested-With"] = "XMLHttpRequest";
+            request.Accept = "*/*";
+            request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+        } // We only need to add post data on a POST request
+        else if (_isPost && !_isPostXhr)
+        {
+            request.ContentType = "application/x-www-form-urlencoded";
+        }
+
+        if (_isPost) AddPostDataTo(request); // We only need to add post data on a POST request
+        return true;
+    }
+
+    /// <summary>
+    /// Event handler for HtmlWeb.PostResponseHandler. Occurs after a HTTP response is received
+    /// </summary>
+    protected void OnAfterResponse(HttpWebRequest request, HttpWebResponse response)
+    {
+        SaveCookiesFrom(response); // Save cookies for subsequent requests
+    }
+
+    /// <summary>
+    /// Event handler for HtmlWeb.PreHandleDocumentHandler. Occurs before a HTML document is handled
+    /// </summary>
+    protected void OnPreHandleDocument(HtmlDocument document)
+    {
+        SaveHtmlDocument(document);
+    }
+
+    /// <summary>
+    /// Assembles the Post data and attaches to the request object
+    /// </summary>
+    private void AddPostDataTo(HttpWebRequest request)
+    {
+        string payload = FormElements.AssemblePostPayload();
+        byte[] buff = Encoding.UTF8.GetBytes(payload.ToCharArray());
+        request.ContentLength = buff.Length;
+
+        System.IO.Stream reqStream = request.GetRequestStream();
+        reqStream.Write(buff, 0, buff.Length);
+    }
+
+    /// <summary>
+    /// Add cookies to the request object
+    /// </summary>
+    private void AddCookiesTo(HttpWebRequest request)
+    {
+        if (Cookies != null && Cookies.Count > 0)
+        {
+            request.CookieContainer.Add(Cookies);
+        }
+    }
+
+    /// <summary>
+    /// Saves cookies from the response object to the local CookieCollection object
+    /// </summary>
+    private void SaveCookiesFrom(HttpWebResponse response)
+    {
+        if (response.Cookies.Count > 0)
+        {
+            if (Cookies == null) Cookies = new CookieCollection();
+            Cookies.Add(response.Cookies);
+        }
+    }
+
+    /// <summary>
+    /// Saves the form elements collection by parsing the HTML document
+    /// </summary>
+    private void SaveHtmlDocument(HtmlDocument document)
+    {
+        _htmlDoc = document;
+        FormElements = new FormElementCollection(_htmlDoc);
+    }
+}
+
+public class FormElementCollection : Dictionary<string, string>
+{
+    /// <summary>
+    /// Constructor. Parses the HtmlDocument to get all form input elements. 
+    /// </summary>
+    public FormElementCollection(HtmlDocument htmlDoc)
+    {
+        var inputs = htmlDoc.DocumentNode.Descendants("input");
+        foreach (var element in inputs)
+        {
+            string name = element.GetAttributeValue("name", "undefined");
+            string value = element.GetAttributeValue("value", "");
+            if (!name.Equals("undefined"))
+            {
+                if (!ContainsKey(name))
+                {
+                    Add(name, value);
+                }
+                else
+                {
+                    this[name] = value;
+                }
+            }
+
+            ;
+        }
+    }
+
+    /// <summary>
+    /// Assembles all form elements and values to POST. Also html encodes the values.  
+    /// </summary>
+    public string AssemblePostPayload()
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (var element in this)
+        {
+            string value = HttpUtility.UrlEncode(element.Value);
+            sb.Append("&" + element.Key + "=" + value);
+        }
+
+        return sb.ToString().Substring(1);
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using ScrapySharp.Html.Forms;
+using ScrapySharp.Network;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var b = new BrowserSession();
+            b.Get("https://www.transavia.com/fr-FR/accueil");
+
+            Thread.Sleep(1000);
+
+            b.FormElements["routeSelection.DepartureStation"] = "ORY";
+            b.FormElements["routeSelection.ArrivalStation"] = "AMS";
+            b.FormElements["dateSelection.OutboundDate.Day"] = "17";
+            b.FormElements["dateSelection.OutboundDate.Month"] = "10";
+            b.FormElements["dateSelection.OutboundDate.Year"] = "2019";
+            b.FormElements["dateSelection.IsReturnFlight"] = "true";
+            b.FormElements["dateSelection.InboundDate.Day"] = "24";
+            b.FormElements["dateSelection.InboundDate.Month"] = "10";
+            b.FormElements["dateSelection.InboundDate.Year"] = "2019";
+            b.FormElements["selectPassengersCount.AdultCount"] = "1";
+            b.FormElements["selectPassengersCount.ChildCount"] = "0";
+            b.FormElements["selectPassengersCount.InfantCount"] = "0";
+            b.FormElements["flyingBlueSearch.FlyingBlueSearch"] = "false";
+
+            var response = b.Post("https://www.transavia.com/fr-FR/reservez-un-vol/vols/deeplink");
+
+            File.WriteAllText($"res{Guid.NewGuid()}.html", response);
+
+            Thread.Sleep(1000);
+
+
+            var handler = new HttpClientHandler();
+            handler.CookieContainer = new CookieContainer();
+            handler.CookieContainer.Add(b.Cookies);
+            var client = new HttpClient(handler);
+            handler.UseCookies = true;
+            // client.DefaultRequestHeaders.Accept = ""
+            client.DefaultRequestHeaders.Add("Accept", "*/*");
+            var ua =
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36";
+            client.DefaultRequestHeaders.Add("X-Requested-With", "XMLHttpRequest");
+
+            client.DefaultRequestHeaders.UserAgent.ParseAdd(ua);
+
+            client.DefaultRequestHeaders.Add("Accept-Language", "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6");
+            var url = "https://www.transavia.com/fr-FR/reservez-un-vol/vols/SingleDayAvailability/";
+
+
+            var dict = new Dictionary<string, string>();
+            dict.Add("selectSingleDayAvailability.JourneyType", "OutboundFlight");
+            dict.Add("selectSingleDayAvailability.Date.DateToParse", "2019-10-17");
+            dict.Add("selectSingleDayAvailability.AutoSelect", "true");
+            dict.Add("__RequestVerificationToken", b.Cookies["__RequestVerificationToken"].Value);
+            var req = new HttpRequestMessage(HttpMethod.Post, url) {Content = new FormUrlEncodedContent(dict)};
+            var res = await client.SendAsync(req);
+            var html = await res.Content.ReadAsStringAsync();
+            var jObject = Newtonsoft.Json.Linq.JObject.Parse(html);
+            var ddd = (string) jObject["SingleDayOutbound"];
+            File.WriteAllText($"OutboundFlight_{Guid.NewGuid()}.html", ddd);
+
+
+            dict = new Dictionary<string, string>();
+            dict.Add("selectSingleDayAvailability.JourneyType", "InboundFlight");
+            dict.Add("selectSingleDayAvailability.Date.DateToParse", "2019-10-24");
+            dict.Add("selectSingleDayAvailability.AutoSelect", "true");
+            dict.Add("__RequestVerificationToken", b.Cookies["__RequestVerificationToken"].Value);
+            req = new HttpRequestMessage(HttpMethod.Post, url) {Content = new FormUrlEncodedContent(dict)};
+            res = await client.SendAsync(req);
+            html = await res.Content.ReadAsStringAsync();
+            jObject = Newtonsoft.Json.Linq.JObject.Parse(html);
+            ddd = (string) jObject["SingleDayInbound"];
+            File.WriteAllText($"InboundFlight_{Guid.NewGuid()}.html", ddd);
+
+            
+            // request.Headers["X-Requested-With"] = "XMLHttpRequest";
+
+            //  request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+
+
+            //var flight = b.PostXhr("https://www.transavia.com/fr-FR/reservez-un-vol/vols/SingleDayAvailability/");
+
+            //https: //www.transavia.com/fr-FR/reservez-un-vol/vols/SingleDayAvailability/
+
+            // File.WriteAllText($"res2{Guid.NewGuid()}.html", flight);
+            /*ScrapingBrowser Browser = new ScrapingBrowser();
+            Browser.AllowAutoRedirect = true; // Browser has settings you can access in setup
+            Browser.AllowMetaRedirect = true;
+            WebPage PageResult = Browser.NavigateToPage(new Uri("https://www.transavia.com/fr-FR"));
+
+            PageWebForm form = PageResult.FindFormById("desktop");
+
+            form["routeSelection.DepartureStation"] = "ORY";
+            form["routeSelection.ArrivalStation"] = "AGA";
+            form["dateSelection.OutboundDate.Day"] = "17";
+            form["dateSelection.OutboundDate.Month"] = "7";
+            form["dateSelection.OutboundDate.Year"] = "2019";
+            form["dateSelection.IsReturnFlight"] = "true";
+            form["dateSelection.InboundDate.Day"] = "24";
+            form["dateSelection.InboundDate.Month"] = "7";
+            form["dateSelection.InboundDate.Year"] = "2019";
+            form["selectPassengersCount.AdultCount"] = "1";
+            form["selectPassengersCount.ChildCount"] = "0";
+            form["selectPassengersCount.InfantCount"] = "0";
+            form["flyingBlueSearch.FlyingBlueSearch"] = "false";
+
+            form.Method = HttpVerb.Post;
+            WebPage resultsPage = form.Submit();
+
+            File.WriteAllText("res.html", resultsPage.Content);*/
+        }
+    }
+}
